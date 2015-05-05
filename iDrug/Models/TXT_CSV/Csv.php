0c27506ab5a864c6a8a8aea9b03db64b1acbb1c0
Choudoor <gauthierfreund@free.fr>
2015-05-05 16:12:13 +0200

    Model txt csv

---
+++ iDrug/Models/TXT_CSV/Csv.php
@@ -0,0 +1,14 @@
+<?php
+	class Csv {
+		
+		private int $Class_ID;
+		var $Preferred_Label;
+		var $Synonyms;
+		var $Definitions;
+		var $Obsolete;
+		var $CUI;
+		var $Semantic_Types;
+		var $Parents;
+		
+	}
+?>
