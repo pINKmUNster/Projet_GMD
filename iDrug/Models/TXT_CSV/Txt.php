817ac360dc8d05597d24f85f69dca38ec34c496b
Choudoor <gauthierfreund@free.fr>
2015-05-12 18:00:13 +0200

    MAJ class TXT

--- iDrug/Models/TXT_CSV/Txt.php
+++ iDrug/Models/TXT_CSV/Txt.php
@@ -5,5 +5,43 @@
 	class Txt
 	{
 		
+		/***********
+		* File TXT *
+		***********/
+		
+		private $lineTxt;
+		private $EOF;
+		
+		
+		/**************
+		* Constructor *
+		**************/
+		
+		public function __construct()
+		{
+			$this->lineTxt = new LineTxt();
+			$this->setEOF(FALSE);
+		}
+		
+		
+		/**********
+		* Setters *
+		**********/
+		
+		public function setEOF($EOFTmp)
+		{
+			$this->EOF = $EOFTmp;
+		}
+		
+		
+		/**********
+		* Getters *
+		**********/
+		
+		public function getEOF()
+		{
+			return $this->EOF;
+		}
+		
 	}
 ?>
