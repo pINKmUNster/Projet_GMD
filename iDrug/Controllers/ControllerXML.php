48091d822b51986f58cb877c7974ecc1a9fd7bbb
anthony fioravanti <anthony.fioravanti@telecomnancy.eu>
2015-05-27 23:59:11 +0200

    merge xml and sql data test

--- iDrug/Controllers/ControllerXML.php
+++ iDrug/Controllers/ControllerXML.php
@@ -1,17 +1,30 @@
 <?php
 require("..\Models\XML\DrugBank.php");
+require("..\Models\SQL\Sql_DAO.php");
+
 $disease = $_POST['disease'];
 $drug = $_POST['drug'];
-$data = new DrugBank($disease,$drug);
 
+//Instance variable;
+$data = new DrugBank($disease,$drug);
+$dao = new DAO(null);
+
+//query
 if(!empty($drug))
 {
 	$data->getDataByDrug();
+	$sqlAdverseMedic=$dao->getAdverse_Medic($drug);
+	$sqlIndicMedic=$dao->getIndic_Medic($drug);
 }
 if(!empty($disease))
 {
 	$data->getDataByDisease();
+	$sqlAdverseDisease = $DAO -> getMedic_Ad($disease);
+	$sqlIndicDisease = $DAO -> getMedic_Indic($disease);
+	
 }
+//Query
+var_dump($sqlIndicMedic);
 
 echo "indication : ".$data->get_drugIndication();
 echo "<br>";
@@ -20,6 +33,22 @@ echo "<br>";
 foreach ($data->get_indication() as $indi)
 {
 	echo $indi ."<br>";
+}
+
+//Function to merge 2 array
+function merge_tab($t1,$t2)
+{	
+	//initialize the counter at size of t1
+	$i = count($t1);
+	foreach($t2 as $t)
+	{
+		if (!(in_array($t, $t2))) 
+		{
+			$t2[i]=$t;
+			$i++;
+		}
+	}
+	return $t1;
 }
 
 ?>
